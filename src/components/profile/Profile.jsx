6d6f5aa2b66b7e8cd077fef1efda938b9be8dacb
Oleksandra K <a.koroliova@gmail.com>
2024-02-26 16:58:42 +0200

    Complete task-1

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,25 +1,29 @@
+import css from './Profile.module.css';
+
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
+    <div className={css.profileContainer}>
       <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <div className={css.avatar}>
+          <img src={image} alt="User avatar" />
+        </div>
+        <p className={css.username}>{name}</p>
+        <p className={css.userTag}>@{tag}</p>
+        <p className={css.userLoc}>{location}</p>
       </div>
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.statContainer}>
+        <li className={css.stat}>
+          <span className={css.statName}>Followers</span>
+          <span className={css.statValue}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.stat}>
+          <span className={css.statName}>Views</span>
+          <span className={css.statValue}>{stats.views}</span>
         </li>
 
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.stat}>
+          <span className={css.statName}>Likes</span>
+          <span className={css.statValue}>{stats.likes}</span>
         </li>
       </ul>
     </div>
