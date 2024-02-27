66a3a41d08cf9a3e7aebca72b77e9c962480ee78
Oleksandra K <a.koroliova@gmail.com>
2024-02-27 21:33:39 +0200

    Switch if to ternary operator in FriendsListItem.jsx

--- src/components/friendlist/FriendListItem.jsx
+++ src/components/friendlist/FriendListItem.jsx
@@ -1,24 +1,26 @@
 import css from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline = false }) => {
-  function friendStatus() {
-    if (isOnline === true) {
-      return (
-        <p className={css.onlineStatus} style={{ color: 'green' }}>
-          Online
-        </p>
-      );
-    }
-    return (
+  const friendStatus = () => {
+    return isOnline === true ? (
+      <p className={css.onlineStatus} style={{ color: 'green' }}>
+        Online
+      </p>
+    ) : (
       <p className={css.onlineStatus} style={{ color: 'red' }}>
         Offline
       </p>
     );
-  }
+  };
 
   return (
     <div>
-      <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
+      <img
+        className={css.avatar}
+        src={avatar}
+        alt="Friend's Avatar"
+        width="48"
+      />
       <p className={css.name}>{name}</p>
       {friendStatus()}
     </div>
